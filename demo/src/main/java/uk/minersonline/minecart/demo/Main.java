1b424cbf93458bae1e8e0769d120f3cd463a3ae2
ajh123 <ajh123@users.noreply.github.com>
2024-02-03 19:17:05 +0000

    feat: initial lwjgl project

---
+++ demo/src/main/java/uk/minersonline/minecart/demo/Main.java
@@ -0,0 +1,7 @@
+package uk.minersonline.minecart.demo;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ engine/src/main/java/uk/minersonline/engine/Main.java
@@ -0,0 +1,7 @@
+package uk.minersonline.engine;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
