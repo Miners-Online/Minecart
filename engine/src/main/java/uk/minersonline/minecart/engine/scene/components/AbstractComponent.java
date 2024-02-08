336d167e989414c8ca52af8f75cf0033375b2fac
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2024-02-08 10:54:59 +0000

    feat: debug ui

--- engine/src/main/java/uk/minersonline/minecart/engine/scene/components/AbstractComponent.java
+++ engine/src/main/java/uk/minersonline/minecart/engine/scene/components/AbstractComponent.java
@@ -1,7 +1,5 @@
 package uk.minersonline.minecart.engine.scene.components;
 
-import uk.minersonline.minecart.engine.scene.Scene;
-
 public abstract class AbstractComponent {
     public abstract void drawGui();
 }
